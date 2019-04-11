777c8f1d6dd7c9782d228a69078c321c61406c08
Brian Summerfield <brianjsummerfield@gmail.com>
2019-04-10 19:28:17 -0500

    add login, logout vues

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Signup from './views/Signup.vue';
+import Login from './views/Login.vue';
+import Logout from './views/Logout.vue';
 
 Vue.use(Router);
 
@@ -22,9 +24,20 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
-    { path: "/signup",
+    { 
+      path: "/signup",
       name: "signup",
       component: Signup  
+    },
+    { 
+      path: "/login",
+      name: "login",
+      component: Login  
+    },
+    { 
+      path: "/logout",
+      name: "logout",
+      component: Logout  
     }
   ]
 });
