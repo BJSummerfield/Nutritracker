4666c9fa0863981d1d0768673ee14c7c64c7dd16
Brian Summerfield <brianjsummerfield@gmail.com>
2019-04-10 20:37:07 -0500

    create index.vue

---
+++ src/views/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h1>Your Meals</h1>
+    <hr>
+    <div v-for="consumption in consumptions">
+      <p>Meal: {{ consumption.meal }}</p>
+      <p>Name: {{ consumption.name }}</p>
+      <p>Protein: {{ consumption.protein }}</p>
+      <p>Sodium: {{ consumption.sodium }}</p>
+      <p>Calories: {{ consumption.energy }}</p>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      consumptions: []
+    };
+  },
+  created: function() {
+    axios.get("/api/consumptions").then(response => {
+      this.consumptions = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
