121dca2d2998dcb39966c8e649fa06975eefaecb
Brian Summerfield <brianjsummerfield@gmail.com>
2019-04-14 15:36:06 -0500

    add measures to food search

--- src/views/FoodsIndex.vue
+++ src/views/FoodsIndex.vue
@@ -6,14 +6,21 @@
       <p>Name: <input type="text" v-model="foodSearch"></p>
       <input type="submit" value="search">
     </form>
-    <!-- <div v-for="consumption in consumptions">
-      <p>Meal: {{ consumption.meal }}</p>
-      <p>Name: {{ consumption.name }}</p>
-      <p>Protein: {{ consumption.protein }}</p>
-      <p>Sodium: {{ consumption.sodium }}</p>
-      <p>Calories: {{ consumption.energy }}</p>
+    <hr>
+    <div v-for="food in foods">
+      <p> {{ food.food['desc']['name'] }}</p>
+      <button v-on:click="currentFood = food">More Info</button>
+      <div v-if="food === currentFood">
+        <div v-for="label in food.food['nutrients'][0]['measures']">
+          <div v-if="label && label.label">
+            <p>{{label.label }}</p>
+          </div>
+        </div>
+      </div>
       <hr>
-    </div> -->
+
+
+    </div>
   </div>
 </template>
 
@@ -25,16 +32,23 @@ export default {
     return {
       foodSearch: "",
       group: "",
-      foods: []
+      foods: [],
+      currentFood: {}
     };
   },
-  created: function() {
-    axios.get("/api/foods").then(response => {
-      console.log(response.data);
-      this.foods = response.data;
-    });
-  },
+  // created: function() {
+  //   axios.get("/api/foods").then(response => {
+  //     console.log(response.data);
+  //     this.foods = response.data;
+  //   });
+  // },
   methods: {
+    moreInfo: function() {
+      console.log(this.information);
+      this.information = this.food['desc']['ndbno'];
+      console.log(this.information);
+    },
+
     searchFood: function(theFood) {
       console.log('searching database');
       var params = {
@@ -46,6 +60,7 @@ export default {
       axios.get("/api/foods?food=" + params.food + "&group=" + params.group, params).then(response => {
         console.log(this.foodSearch);
         console.log(response.data);
+        this.foods = response.data;
       });
     }
   }
